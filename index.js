c0c34275c0dc0bec42973ce423a8695753804d32
Valvas <valvas.vincent@outlook.fr>
2018-06-15 15:35:49 +0200

    Cross-domain activated

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ const connection = mysql.createConnection(
   insecureAuth  : true
 });
 
+app.use((req, res, next) =>
+{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 app.set('connection', connection);
 
 app.listen(3000, () => 
