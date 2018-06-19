5be2d956fb10a7f7af570134d049fa3e3da24f97
Hexus <valvas.vincent@outlook.fr>
2018-06-19 23:53:29 +0200

    Working on add element

--- functions.js
+++ functions.js
@@ -5,7 +5,6 @@ const messages  = require('./messages');
 const jwt       = require('jsonwebtoken');
 
 /****************************************************************************************************/
-
 module.exports.obtainNewToken = (token, callback) =>
 {
     jwt.verify(token, params.secretKey, (error, decoded) =>
@@ -18,6 +17,35 @@ module.exports.obtainNewToken = (token, callback) =>
 
             return callback(null, token);
         });
+    });
+}
+
+/****************************************************************************************************/
+
+module.exports.getEmailFromToken = (token, callback) =>
+{
+    jwt.verify(token, params.secretKey, (error, decoded) =>
+    {
+        if(error) return callback({ status: 406, message: messages.TOKEN_READING_ERROR, detail: error.message });
+
+        else
+        {
+            return callback(null, decoded.email);
+        }
+    });
+}
+
+/****************************************************************************************************/
+
+module.exports.getAccountFromEmail = (emailAddress, connection, callback) =>
+{
+    connection.query(`SELECT * FROM users WHERE MAIL = "${emailAddress}"`, (error, result) =>
+    {
+        if(error) return callback({ status: 500, message: messages.DATABASE_ERROR, detail: error.message });
+
+        if(result.length == 0) return callback({ status: 406, message: messages.ACCOUNT_DOES_NOT_EXIST, detail: null });
+
+        return callback(null, result[0]);
     });
 }
 
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mysql           = require('mysql');
 let app = express();
 
 app.use(cookieParser());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) =>
