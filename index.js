c1e6dffb54db1401bd15421214a4bedd2103f1ee
Valvas <valvas.vincent@outlook.fr>
2018-07-11 19:27:11 +0200

    Modify bodyparser cache

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: '5mb' }));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true} ));
 
 app.use((req, res, next) =>
 {
